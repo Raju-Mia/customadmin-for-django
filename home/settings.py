292b7b02d8bd0aa305710df434e2dd1e06f0d681
Raju-Mia <rajucse00@gmail.com>
2022-12-29 22:00:57 +0600

    Django 404 (page not found)

--- home/settings.py
+++ home/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&-1i-)!)&y7(8=-xh@s$7y-a9uh*+w24#&kwwr64s9t29nx^%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
